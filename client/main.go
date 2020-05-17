fe8aecc2625a14a82bebf12b62f90f4ae672b0e1
Arianna Vespri <arianna.vespri@yahoo.it>
2020-05-17 14:49:39 +0200

    Complete client

--- client/main.go
+++ client/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
+	"os"
+	"strconv"
 	"time"
 
 	pb "github.com/vesari/grpccalc/grpccalc"
@@ -13,20 +16,71 @@ const (
 	address = "localhost:50051"
 )
 
-func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+func add(ctx context.Context) {
+	if len(os.Args) != 4 {
+		log.Fatal("You have to provide 3 arguments")
 	}
+	operand1, err := strconv.ParseInt(os.Args[2], 10, 64)
+	if err != nil {
+		log.Fatal("Unable to convert first operand to an integer")
+	}
+	operand2, err := strconv.ParseInt(os.Args[3], 10, 64)
+	if err != nil {
+		log.Fatal("Unable to convert second operand to an integer")
+	}
+	c, conn := newClient()
 	defer conn.Close()
-	c := pb.NewCalcClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.Add(ctx, &pb.AddRequest{Number1: 1, Number2: 45})
+	r, err := c.Add(ctx, &pb.AddRequest{Number1: operand1, Number2: operand2})
 	if err != nil {
 		log.Fatalf("could not add: %v", err)
 	}
 	log.Printf("Result: %d", r.GetValue())
+}
+
+func newClient() (pb.CalcClient, io.Closer) {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	c := pb.NewCalcClient(conn)
+
+	return c, conn
+}
+
+func multiplyF(ctx context.Context) {
+	if len(os.Args) != 4 {
+		log.Fatal("You have to provide 3 arguments")
+	}
+	operand1, err := strconv.ParseFloat(os.Args[2], 64)
+	if err != nil {
+		log.Fatal("Unable to convert first operand to a float")
+	}
+	operand2, err := strconv.ParseFloat(os.Args[3], 64)
+	if err != nil {
+		log.Fatal("Unable to convert second operand to a float")
+	}
+	c, conn := newClient()
+	defer conn.Close()
+	r, err := c.MultiplyF(ctx, &pb.MultiplyFRequest{Number1: operand1, Number2: operand2})
+	if err != nil {
+		log.Fatalf("could not multiply: %v", err)
+	}
+	log.Printf("Result: %.2f", r.GetValue())
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Not enough arguments")
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	op := os.Args[1]
+	switch op {
+	case "add":
+		add(ctx)
+	case "multiplyF":
+		multiplyF(ctx)
+	default:
+		log.Fatalf("Invalid operator %q", op)
+	}
 }
